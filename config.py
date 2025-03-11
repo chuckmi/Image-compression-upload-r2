b23244271a9ca4babb25b728b7b12240c8000c5a
Chuck <chuckmi@outlook.com>
2025-03-11 21:13:10 +0800

    update

--- config.py
+++
@@ -1,7 +0,0 @@
-R2_CONFIG = {
-    'endpoint_url': 'https://cb909aba4bc12157417c84091602de13.r2.cloudflarestorage.com',  # 从 R2 控制面板获取的 S3 API 终端节点
-    'aws_access_key_id': '20b04261ae2525599fa2123f0e899f37',
-    'aws_secret_access_key': '7b75b9bd1209a6b77b8dcad0dc8ac22d51e1eddae19682e2ec64de998bf495c0',
-    'bucket_name': 'pandatrips',
-    'custom_domain': 'cdn.pandatrips.com'  # 可选
-} 
